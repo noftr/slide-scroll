4686574b82f77d4c49bf9820a7397812541b94b5
YuliaKhodunova <hodunovay@gmail.com>
2022-06-27 18:05:14 +0300

    вывела в консоль момент нажатия

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,15 @@
 // test
 alert('aaaaaaa');
+
+let touch_up = 0;
+let touch_down = 0;
+let touch_path = 0;
+
+
+
+function point_down() {
+  console.log('Произошло нажатие');
+}
+
+
+window.addEventListener('touchstart', point_down);
