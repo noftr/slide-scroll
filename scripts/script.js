d62ca2d4f3754ec19dd843a60beae47859b6c706
YuliaKhodunova <hodunovay@gmail.com>
2022-06-29 16:08:22 +0300

    сделали задержку на 1 сек

--- scripts/script.js
+++ scripts/script.js
@@ -5,7 +5,8 @@ let touch_down = 0;
 let touch_path = 0;
 let coord_current = 0;
 let coord_last = 0;
-
+let touch_once = true;
+let time = 1000
 
 window.addEventListener('touchstart', point_down);
 window.addEventListener('touchend', point_cancel);
@@ -24,15 +25,25 @@ function pointer_move(e) {
     coord_current = e.touches[0].clientY;
     touch_path = coord_current - coord_last;
     coord_last = e.touches[0].clientY;
-    if (touch_path<0) {
-        console.log("скролю вниз")
-};
-   if (touch_path>0) {
-        console.log("скролю вверх")
+    if (touch_once == true) {
+        if (touch_path<0) {
+            console.log("скролю вниз")
+        } else {
+            console.log("скролю вверх")
+        };
+        touch_once = false;
+
+        setTimeout(function() {
+            touch_once = true;
+        }, 1000)
+
+
+    }
+
 };
 
-    //console.log(touch_path);
-}
+
+
 
 function onWheel() {
     console.log('Крутим колесико');
