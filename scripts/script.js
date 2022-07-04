529f4bd78966cfd6efcf369eceb61ba4a57a7a03
YuliaKhodunova <hodunovay@gmail.com>
2022-07-04 15:51:43 +0300

    Перемещение от 1 до 4 секции

--- scripts/script.js
+++ scripts/script.js
@@ -1,49 +1,55 @@
 
 
-let touch_up = 0;
-let touch_down = 0;
 let touch_path = 0;
 let coord_current = 0;
 let coord_last = 0;
 let touch_once = true;
-let time = 1000
 
-window.addEventListener('touchstart', point_down);
-window.addEventListener('touchend', point_cancel);
+//et section3 = document.getElementById('section3');
+let anchor1;
+let anchor_num = 0;
+
 window.addEventListener('touchmove', pointer_move);
 window.addEventListener('wheel', onWheel);
-
-function point_down() {
-    console.log('Коснулись экрана');
-}
-
-function point_cancel() {
-    console.log('Убрали палец');
-}
 
 function pointer_move(e) {
     coord_current = e.touches[0].clientY;
     touch_path = coord_current - coord_last;
     coord_last = e.touches[0].clientY;
     if (touch_once == true) {
-        if (touch_path<0) {
-            console.log("скролю вниз")
+        if (touch_path>0) {
+            if (anchor_num<3) {
+
+                anchor_num +=1;
+                anchor1 = document.querySelectorAll(".anchor")[anchor_num];
+                anchor1.scrollIntoView();
+                console.log("мы сейчас на"+" "+(anchor_num+1)+" "+"секции");
+
+            }
         } else {
-            console.log("скролю вверх")
+            if (anchor_num>=1) {
+
+                anchor_num -=1;
+                anchor1 = document.querySelectorAll(".anchor")[anchor_num];
+                anchor1.scrollIntoView();
+                console.log("мы сейчас на"+" "+(anchor_num+1)+" "+"секции");
+
+            }
         };
         touch_once = false;
 
         setTimeout(function() {
             touch_once = true;
-        }, 1000)
-
+        }, 400)
 
     }
-
 };
 
-
-
+//section3.scrollIntoView();
+//elem.scrollTo('section3');
+//function scrollBy() {
+//    elem.scrollBy('section3');
+//}
 
 function onWheel() {
     console.log('Крутим колесико');
